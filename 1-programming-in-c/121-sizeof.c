df8ed10c8f10b812a90a2c8a6da4a60f07432b1d
Daria Mashkova <dmetafrasis@gmail.com>
2024-09-14 18:07:38 +0700

    Update 121-sizeof.c

--- 1-programming-in-c/121-sizeof.c
+++ 1-programming-in-c/121-sizeof.c
@@ -1,18 +1,25 @@
-/* The statement
-samp = (short) fracsamp * 32767;
-is intended to convert a floating-point sample (e1.0) to the 16-bit integer range:
-samp = (short) fracsamp * 32767;
-However the output is not as expected. Why?
-
-Ans: undeclared variables
+/* Using the sizeof() operator and printf statements, 
+write a small program to report the size in bytes of each of the following:
+int char short long float double
+This will be of particular interest and importance if you happen to be using a 64-bit machine.
 */
+
 #include <stdio.h>
 
 int main()
 {
-	short fracsamp;
-	int samp = fracsamp * 32767;
+	int intsize = sizeof(int);
+	int charsize = sizeof(char);
+	int shortsize = sizeof(short);
+	int longsize = sizeof(long); 
+	int floatsize = sizeof(float);
+	int doublesize = sizeof(double);
 
-	printf("%d\n", samp);
+	printf("int size is %d byte(s)\n", intsize);
+	printf("char size is %d byte(s)\n", charsize);
+	printf("short size is %d byte(s)\n", shortsize);
+	printf("long size is %d byte(s)\n", longsize);
+	printf("float size is %d byte(s)\n", floatsize);
+	printf("double size is %d byte(s)\n", doublesize);
 	return 0;
 }
